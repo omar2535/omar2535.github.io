4323a893f8e1df03d61192935730706a092060da
Omar Tsai <omar2535@gmail.com>
2020-02-21 10:36:07 -0800

    adding homepage and scrolling text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,40 @@
 <template>
   <div>
-    <h1>HOME </h1>
+    <marquee scrollamount= "5" class="scrolling-text" behavior="scroll" direction="left">{{ hello }}</marquee>
+    <h3 id="title">Welcome</h3>
+    <div id="content">
+      <p>To visit my blog, click <router-link to="/blog">blog</router-link></p>
+      <p>To know more about me, <router-link to="/about">about me</router-link></p>
+    </div>
   </div>
 </template>
 
 <script>
+import multilanguage_hello_json from "../../public/multilanguage-hello.json";
 
+// convert the multi-langauge hello's to a string for the scrolling text
+const multilanguage_hellos = multilanguage_hello_json.map(e => e.hello);
+const multiLanguageHellosString = multilanguage_hellos.join("  ");
+
+console.log(multilanguage_hellos);
 export default {
-  name: "Home"
+  name: "Home",
+  data: () => {
+    return {
+      hello: multiLanguageHellosString
+    }
+  }
 }
+
 </script>
 
-<style>
+<style scoped>
+  #title {
+    text-align: center;
+    padding-top: 1em;
+  }
+  #content {
+    text-align: center;
+    padding: 1em;
+  }
 </style>
