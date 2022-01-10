2bedb5bc679bf19db2d8acbbcd055ab85a57bb7f
omar2535 <omar2535@gmail.com>
2022-01-10 15:54:22 -0800

    update about me

--- .vuepress/components/About.vue
+++ .vuepress/components/About.vue
@@ -1,18 +1,29 @@
 <template>
   <div id="about-me">
     <div id="profile-heading">
-      <img id="profile_image" src="../content/profile_image.jpg">
+      <img id="profile_image" src="../content/website_pic.jpg">
       <div id="profile-heading-text">
         <h1 id="name"> Omar Tsai </h1>
         <a :href="'//' + linkedinUrl" target="_blank">Linkedin</a>
         <a :href="'//' + githubUrl" target="_blank"> Github</a>
         <a :href="'//' + twitterUrl" target="_blank">Twitter</a>
       </div>
+      <!-- <div id="certs-div">
+        <a href="https://www.credly.com/badges/d36c34f0-3133-4dbf-8d1b-9cd7d317ef09/public_url" target="_blank">
+          <img class="cert-icon" src="../content/oswe.png"/>
+        </a>
+        <a href="https://www.credly.com/badges/60287ace-0874-4fc5-83ce-72a025cd9736/public_url" target="_blank">
+          <img class="cert-icon" src="../content/oscp.png"/>
+        </a>
+        <a href="https://www.credly.com/badges/1004eff7-e62a-4126-8494-570debaf29dd/public_url" target="_blank">
+          <img class="cert-icon" src="../content/awscsaa.png"/>
+        </a>
+      </div> -->
     </div>
     <div id="body">
-      <p>Hi there, I'm Omar! I'm a new grad from the University of British Columbia. In my free time, I love to code 
-         and learn about a variety of subjects such as investing
-         and low level hardware. I also love to listen to podcasts and audio books.
+      <p>Hi there, I'm Omar! I'm currently doing my masters at Simon Fraser University studying Cybersecurity. 
+        In my free time, I love to code and learn about a variety of subjects such as investing
+         and low level hardware. I also love to listen to podcasts and audio books. 
       </p>
     </div>
   </div>
@@ -35,18 +46,21 @@ export default {
 
 <style scoped>
   #profile_image{
-    width: 200px;
+    width: 500px;
     height: auto;
     border-radius: 10%;
     display:inline-block;
     vertical-align:top;
   }
+
   #profile-heading-text{
     display:inline-block;
     padding: 70px 20px;
+    padding-bottom: 2px;
   }
   #name{
     margin: auto;
+    padding-bottom: 10%;
   }
   #profile-heading{
     width: 100%;
@@ -59,6 +73,13 @@ export default {
   #body{
     padding-left: 20%;
     padding-right: 20%;
+  }
+  .cert-icon{
+    width: 70px;
+    height: 70px;
+  }
+  #certs-div{
+    padding-top: 0%;
   }
   a {
     color: inherit;
